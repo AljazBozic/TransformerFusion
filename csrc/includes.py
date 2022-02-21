24f841077e86d0de0e4616fa35a3bd355d648285
aljaz.bozic@gmail.com <aljaz.bozic@gmail.com>
2022-02-21 12:30:23 +0100

    Removed unnecessary includes.py file

--- csrc/includes.py
+++
@@ -1,8 +0,0 @@
-import os
-
-home_dir = os.path.expanduser("~")
-
-EXTERNAL_LIB_DIR = os.path.join(home_dir, "lib") 
-EIGEN_INCLUDE_DIR = os.path.join(EXTERNAL_LIB_DIR, "eigen3")
-
-# EIGEN_INCLUDE_DIR = "/rhome/abozic/lib/eigen3"
