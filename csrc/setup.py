fe64be2e57a064403d6e0bf170ce42afcab1ab9f
aljaz.bozic@gmail.com <aljaz.bozic@gmail.com>
2022-02-21 16:32:40 +0100

    Fixed setup.py after includes.py removal

--- csrc/setup.py
+++ csrc/setup.py
@@ -11,8 +11,6 @@ from torch.utils.cpp_extension import CUDA_HOME
 from torch.utils.cpp_extension import CppExtension
 from torch.utils.cpp_extension import CUDAExtension
 
-import includes
-
 requirements = ["torch", "torchvision"]
 
 
